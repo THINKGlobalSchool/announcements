3ff07dc7c184bebc96406d265947bba8ddacf93c
Jeff Tilson <jrtilson@gmail.com>
2013-10-23 19:28:58 +0000

    Added widget files

---
+++ views/default/widgets/announcements/content.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Announcements Widget
+ * 
+ * @package Announcements
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010 - 2013
+ * @link http://www.thinkglobalschool.com/
+ */
+
+echo elgg_view('announcements/announcement_list');
